2d68116c6606227992a170f4fff7ffa85dec034b
Viachaslau Kitsun <Viachaslau_Kitsun@epam.com>
2019-03-19 09:48:22 +0300

    Add project files.

---
+++ NetMentoring_noSQL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetMentoring_noSQL
+{
+    public class Class1
+    {
+    }
+}
